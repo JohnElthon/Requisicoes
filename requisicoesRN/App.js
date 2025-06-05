860779bf7c5076c15577b608dd2656d99f04608f
JohnElthon <johnelthon1@gmail.com>
2025-06-05 11:52:47 -0300

    .

--- requisicoesRN/App.js
+++ requisicoesRN/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import api  from './src/services/api';
 
